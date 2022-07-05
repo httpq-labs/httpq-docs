0959afa71011393cb2094935ef3e8333a9b08809
Edward Swiac <eswiac@fastmail.com>
2022-07-04 23:33:47 -0700

    pages

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,19 +4,19 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Vuepress Docs Boilerplate',
+  title: 'HTTPQ',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
   description: description,
-
+  base: "/httpq-docs/",
   /**
    * Extra tags to be injected to the page HTML `<head>`
    *
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#346c54' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
